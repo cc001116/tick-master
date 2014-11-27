fe67e4f4ec473f4dba4fac85bc2d831db3f39d45
chen <cc001116@126.com>
2014-11-27 21:50:10 +0800

    add test mothed

--- service/src/test/java/cn/flower/tick/service/TestService.java
+++ service/src/test/java/cn/flower/tick/service/TestService.java
@@ -1,5 +1,7 @@
 package cn.flower.tick.service;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,34 @@ public class TestService {
 		User user = new User();
 		user.setUsername("ssssssss");
 		userService.register(user);
+		System.out.println(user.getId());
 	}
+	
+	
+	
+	@Test
+	public void querySingle() {
+		//参数为你执行上一条测试时 控制台打印的ID  可测；
+		User user = userService.query(6L);
+		if(user == null) {
+			System.out.println("对象不存在， 敢快看看数据库中有木有");
+			return ;
+		}
+		System.out.println("用户名：" + user.getUsername()+" 时间："+user.getRegisterDate());
+	}
+	
+	@Test
+	public void update() {
+		User user = userService.query(6L);
+		user.setRegisterDate(new Date());
+		userService.saveOrUpdate(user);
+	}
+	
+	@Test
+	public void delete() {
+		userService.delete(5L);
+	}
+	
+	
 	
 }
